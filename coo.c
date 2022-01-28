747b4887a9560252e36965e559ed5c12d211f58a
agdas <anand.das@gmail.com>
2022-01-28 15:40:46 -0800

    Update coo.c
    taking in changes from Anand

--- coo.c
+++ coo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(int argc, char **argv) {
-	int counter=0; /* counter*/
+	int i=0; /* counter*/
 	/* Print Hello Universe multiple times */
 	for (counter=1; counter<20; counter++) {
 		fprintf(stdout, "Hello Universe!\n");
