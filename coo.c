216b8c53f23f0fdb744b56881a592f51cc3ecf8b
Anand Das <anand.das@gmail.com>
2022-01-27 21:58:53 -0800

    updated coo.c to say hello universe

--- coo.c
+++ coo.c
@@ -3,7 +3,7 @@ int main(int argc, char **argv) {
 	int counter=0; /* counter*/
 	/* Print Hello World multiple times */
 	for (counter=1; counter<20; counter++) {
-		fprintf(stdout, "Hello World!\n");
+		fprintf(stdout, "Hello Universe!\n");
 	}
 	return 0;
 }
