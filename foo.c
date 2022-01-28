35992ec81a10274903886a38a2d708c08011abc7
Anand Das <anand.das@gmail.com>
2022-01-27 16:13:20 -0800

    updated the number from 100 to 20

--- foo.c
+++ foo.c
@@ -2,7 +2,7 @@
 int main(int argc, char **argv) {
 	int counter=0; /* counter*/
 	/* Print Hello World multiple times */
-	for (counter=1; counter<100; counter++) {
+	for (counter=1; counter<20; counter++) {
 		fprintf(stdout, "Hello World!\n");
 	}
 	return 0;
