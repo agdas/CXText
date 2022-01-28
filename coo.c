d456b65ad610fd4b3bd5db83f8c98e583c4e044b
agdas <anand.das@gmail.com>
2022-01-28 15:45:50 -0800

    Test pull request (#2)
    * Fixed issues left in the existing code.
    
    * Update foo.c
    
    i changed to counter
    
    * Update foo.c
    
    updated i to counter where used
    
    * updated the number from 100 to 20
    
    * Adding coo.c
    
    * updated coo.c to say hello universe
    
    * Updated the comment
    
    * Update coo.c
    
    taking in changes from Anand
    
    * Update coo.c
    
    accepted

---
+++ coo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char **argv) {
+	int i=0; /* counter*/
+	/* Print Hello Universe multiple times */
+	for (i=1; i<20; i++) {
+		fprintf(stdout, "Hello Universe!\n");
+	}
+	return 0;
+}
