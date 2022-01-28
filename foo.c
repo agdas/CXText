844d24bf87b85d638b1a451707a7e3b2c476a0f8
agdas <anand.das@gmail.com>
2022-01-27 16:15:29 -0800

    Fixed issues left in the existing code. (#1)
    * Fixed issues left in the existing code.
    
    * Update foo.c
    
    i changed to counter
    
    * Update foo.c
    
    updated i to counter where used
    
    * updated the number from 100 to 20

--- foo.c
+++ foo.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
- int main(int argc, char **argv) {
-   int i;
+int main(int argc, char **argv) {
+	int counter=0; /* counter*/
 	/* Print Hello World multiple times */
-   for (i=1; i<100; i++) {
-    	printf(stdout, "Hello World!\n");
-   }
- }
+	for (counter=1; counter<20; counter++) {
+		fprintf(stdout, "Hello World!\n");
+	}
+	return 0;
+}
