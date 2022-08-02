559822ccf80ba177649870b0e7a2ccf03a9b5ff6
agdas <anand.das@gmail.com>
2022-08-01 18:28:37 -0700

    Update coo.c

--- coo.c
+++ coo.c
@@ -3,7 +3,7 @@ int main(int argc, char **argv) {
 	int i=0; /* counter*/
 	/* Print Hello Universe multiple times */
 	for (i=1; i<20; i++) {
-		fprintf(stdout, "Hello Universe!\n");
+		fprintf(stdout, "Yo, Hello Universe!\n");
 	}
 	return 0;
 }
